310a5047ea29022451c155a2d6bc9785540588f4
tayusa <37957375+tayusa@users.noreply.github.com>
2019-01-29 23:50:18 +0900

    [add] coordinateモデル

---
+++ app/domain/coordinate.go
@@ -0,0 +1,8 @@
+package domain
+
+type Coordinate struct {
+	Id        int      `json:"id"`
+	Image     string   `json:"image"`
+	UserId    string   `json:"userId"`
+	CreatedAt JsonTime `json:"createdAt"`
+}
