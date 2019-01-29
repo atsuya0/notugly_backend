1c1956f85fbc76917e968df06aef78ef83200fcc
tayusa <37957375+tayusa@users.noreply.github.com>
2019-01-29 23:57:27 +0900

    [add] mysqlとの接続を作る関数

---
+++ app/infrastructure/repository/mysql.go
@@ -0,0 +1,27 @@
+package repository
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/tayusa/notugly/app/config"
+)
+
+func NewMySql() *sql.DB {
+	dataSourceName := fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?parseTime=true&loc=Local",
+		config.Data.DB.User,
+		config.Data.DB.Password,
+		config.Data.DB.Host,
+		config.Data.DB.Port,
+		config.Data.DB.Name,
+	)
+
+	conn, err := sql.Open("mysql", dataSourceName)
+	if err != nil {
+		panic(err)
+	}
+
+	return conn
+}
