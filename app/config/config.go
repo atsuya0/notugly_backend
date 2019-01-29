50d43e9a79fbd3594b44f26495db50f626377147
tayusa <37957375+tayusa@users.noreply.github.com>
2019-01-29 22:30:36 +0900

    [add] config

---
+++ app/config/config.go
@@ -0,0 +1,46 @@
+package config
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+)
+
+const message = "Config Error"
+
+type backend struct {
+	Port string `json:"port"`
+}
+
+type frontend struct {
+	Host string `json:"host"`
+	Port string `json:"port"`
+}
+
+type database struct {
+	Name     string `json:"name"`
+	User     string `json:"user"`
+	Password string `json:"password"`
+	Host     string `json:"host"`
+	Port     string `json:"port"`
+}
+
+type config struct {
+	Backend  backend  `json:"backend"`
+	Frontend frontend `json:"frontend"`
+	DB       database `json:"database"`
+}
+
+var Data config
+
+func LoadConfig() {
+	file, err := os.Open("./app/config.json")
+	if err != nil {
+		log.Fatalln(message, err)
+	}
+
+	decoder := json.NewDecoder(file)
+	if err = decoder.Decode(&Data); err != nil {
+		log.Fatalln(message, err)
+	}
+}
