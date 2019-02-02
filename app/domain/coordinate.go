6efa861c90c9bd2ccb6473d49db6518617055a09
tayusa <37957375+tayusa@users.noreply.github.com>
2019-02-03 00:06:46 +0900

    [add] いいね済みか

--- app/domain/coordinate.go
+++ app/domain/coordinate.go
@@ -1,9 +1,10 @@
 package domain
 
 type Coordinate struct {
-	Id        int      `json:"id"`
-	Image     string   `json:"image"`
-	UserId    string   `json:"userId"`
-	CreatedAt JsonTime `json:"createdAt"`
-	Favorites int      `json:"favorites"`
+	Id          int      `json:"id"`
+	Image       string   `json:"image"`
+	UserId      string   `json:"userId"`
+	CreatedAt   JsonTime `json:"createdAt"`
+	Favorites   int      `json:"favorites"`
+	IsFavorited bool     `json:"isFavorited"`
 }
