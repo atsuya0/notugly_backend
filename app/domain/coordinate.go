3c7ba72b314f9b18b9a22b86bd12ad077fd90804
tayusa <37957375+tayusa@users.noreply.github.com>
2019-02-04 15:52:30 +0900

    [update] エンコードされた画像を入れるフィールドを追加

--- app/domain/coordinate.go
+++ app/domain/coordinate.go
@@ -3,6 +3,7 @@ package domain
 type Coordinate struct {
 	Id          int      `json:"id"`
 	ImageName   string   `json:"imageName"`
+	Image       string   `json:"image"`
 	UserId      string   `json:"userId"`
 	CreatedAt   JsonTime `json:"createdAt"`
 	Favorites   int      `json:"favorites"`
