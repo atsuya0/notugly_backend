747cfe9eade803ab4fa7384cafc07b6c894423a6
tayusa <37957375+tayusa@users.noreply.github.com>
2019-02-07 16:20:23 +0900

    [update] 組み立て用の関数を除去

--- app/registry/registry.go
+++ app/registry/registry.go
@@ -7,8 +7,6 @@ import (
 	"github.com/tayusa/notugly_backend/app/infrastructure/repository"
 	"github.com/tayusa/notugly_backend/app/interface/controller"
 	"github.com/tayusa/notugly_backend/app/interface/presenter"
-	output "github.com/tayusa/notugly_backend/app/usecase/presenter"
-	input "github.com/tayusa/notugly_backend/app/usecase/repository"
 	"github.com/tayusa/notugly_backend/app/usecase/service"
 )
 
@@ -29,61 +27,25 @@ func (i *interactor) NewAppHandler() handler.AppHandler {
 		i.NewUserHandler(), i.NewCoordinateHandler(), i.NewFavoriteHandler())
 }
 
-// user
 func (i *interactor) NewUserHandler() handler.UserHandler {
-	return handler.NewUserHandler(i.NewUserController())
+	return handler.NewUserHandler(
+		controller.NewUserController(
+			service.NewUserService(
+				repository.NewUserRepository(i.db),
+				presenter.NewUserPresenter())))
 }
 
-func (i *interactor) NewUserController() controller.UserController {
-	return controller.NewUserController(i.NewUserService())
-}
-
-func (i *interactor) NewUserService() service.UserService {
-	return service.NewUserService(i.NewUserRepository(), i.NewUserPresenter())
-}
-
-func (i *interactor) NewUserRepository() input.UserRepository {
-	return repository.NewUserRepository(i.db)
-}
-
-func (i *interactor) NewUserPresenter() output.UserPresenter {
-	return presenter.NewUserPresenter()
-}
-
-// coordinate
 func (i *interactor) NewCoordinateHandler() handler.CoordinateHandler {
-	return handler.NewCoordinateHandler(i.NewCoordinateController())
+	return handler.NewCoordinateHandler(
+		controller.NewCoordinateController(
+			service.NewCoordinateService(
+				repository.NewCoordinateRepository(i.db),
+				presenter.NewCoordinatePresenter())))
 }
 
-func (i *interactor) NewCoordinateController() controller.CoordinateController {
-	return controller.NewCoordinateController(i.NewCoordinateService())
-}
-
-func (i *interactor) NewCoordinateService() service.CoordinateService {
-	return service.NewCoordinateService(i.NewCoordinateRepository(), i.NewCoordinatePresenter())
-}
-
-func (i *interactor) NewCoordinateRepository() input.CoordinateRepository {
-	return repository.NewCoordinateRepository(i.db)
-}
-
-func (i *interactor) NewCoordinatePresenter() output.CoordinatePresenter {
-	return presenter.NewCoordinatePresenter()
-}
-
-// favorite
 func (i *interactor) NewFavoriteHandler() handler.FavoriteHandler {
-	return handler.NewFavoriteHandler(i.NewFavoriteController())
-}
-
-func (i *interactor) NewFavoriteController() controller.FavoriteController {
-	return controller.NewFavoriteController(i.NewFavoriteService())
-}
-
-func (i *interactor) NewFavoriteService() service.FavoriteService {
-	return service.NewFavoriteService(i.NewFavoriteRepository())
-}
-
-func (i *interactor) NewFavoriteRepository() input.FavoriteRepository {
-	return repository.NewFavoriteRepository(i.db)
+	return handler.NewFavoriteHandler(
+		controller.NewFavoriteController(
+			service.NewFavoriteService(
+				repository.NewFavoriteRepository(i.db))))
 }
