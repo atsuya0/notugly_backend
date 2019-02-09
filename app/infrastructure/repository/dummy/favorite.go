e7415df64bc718b12626cb4e0455fd53dd595342
tayusa <37957375+tayusa@users.noreply.github.com>
2019-02-09 17:05:00 +0900

    [add] databaseの代わりにするdummyのrepository

---
+++ app/infrastructure/repository/dummy/favorite.go
@@ -0,0 +1,37 @@
+package dummy
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"path/filepath"
+
+	"github.com/tayusa/notugly_backend/app/domain"
+)
+
+type dummyFavoriteRepository struct {
+}
+
+func (f *dummyFavoriteRepository) Store(_ domain.Favorite) error {
+	return nil
+}
+
+func (f *dummyFavoriteRepository) Delete(_ domain.Favorite) error {
+	return nil
+}
+
+func NewDummyFavoriteRepository() *dummyFavoriteRepository {
+	return &dummyFavoriteRepository{}
+}
+
+func GetFavorites(method int) ([]domain.Favorite, error) {
+	bytes, err := ioutil.ReadFile(filepath.Join("testdata/json/favorites", methods[method]+".json"))
+	if err != nil {
+		return []domain.Favorite{}, err
+	}
+
+	var favorites []domain.Favorite
+	if err = json.Unmarshal(bytes, &favorites); err != nil {
+		return []domain.Favorite{}, err
+	}
+	return favorites, nil
+}
