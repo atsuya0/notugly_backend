b83027697ab3338efd24bbdda7ba3f6cff3c71b8
tayusa <37957375+tayusa@users.noreply.github.com>
2019-03-05 01:40:19 +0900

    [update] change favoriteRepository to use context

--- src/internal/infrastructure/repository/mysql/favorite.go
+++ src/internal/infrastructure/repository/mysql/favorite.go
@@ -1,6 +1,7 @@
 package mysql
 
 import (
+	"context"
 	"database/sql"
 
 	"github.com/tayusa/notugly_backend/internal/domain"
@@ -10,15 +11,15 @@ type favoriteRepository struct {
 	db *sql.DB
 }
 
-func (f *favoriteRepository) Store(favorite domain.Favorite) (err error) {
-	_, err = f.db.Exec(
+func (f *favoriteRepository) Store(ctx context.Context, favorite domain.Favorite) (err error) {
+	_, err = f.db.ExecContext(ctx,
 		"INSERT INTO favorites(coordinate_id, user_id) VALUES(?, ?)",
 		favorite.CoordinateId, favorite.UserId)
 	return
 }
 
-func (f *favoriteRepository) Delete(favorite domain.Favorite) (err error) {
-	_, err = f.db.Exec(
+func (f *favoriteRepository) Delete(ctx context.Context, favorite domain.Favorite) (err error) {
+	_, err = f.db.ExecContext(ctx,
 		"DELETE FROM favorites WHERE coordinate_id = ? AND user_id = ?",
 		favorite.CoordinateId, favorite.UserId)
 	return
