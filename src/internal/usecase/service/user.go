93e58d0a14413f27f4ee8958da13bb1450ff2ce7
tayusa <37957375+tayusa@users.noreply.github.com>
2019-03-05 01:44:13 +0900

    [update] UserService interfaceがcontextを使用するように変更

--- src/internal/usecase/service/user.go
+++ src/internal/usecase/service/user.go
@@ -1,6 +1,8 @@
 package service
 
 import (
+	"context"
+
 	"github.com/tayusa/notugly_backend/internal/domain"
 	"github.com/tayusa/notugly_backend/internal/usecase/presenter"
 	"github.com/tayusa/notugly_backend/internal/usecase/repository"
@@ -12,13 +14,13 @@ type userService struct {
 }
 
 type UserService interface {
-	Get(string) ([]byte, error)
-	Create(domain.User) error
-	Update(domain.User) error
+	Get(context.Context, string) ([]byte, error)
+	Create(context.Context, domain.User) error
+	Update(context.Context, domain.User) error
 }
 
-func (u *userService) Get(uid string) ([]byte, error) {
-	user, err := u.UserRepository.FindById(uid)
+func (u *userService) Get(ctx context.Context, uid string) ([]byte, error) {
+	user, err := u.UserRepository.FindById(ctx, uid)
 	if err != nil {
 		return []byte{}, err
 	}
@@ -31,13 +33,13 @@ func (u *userService) Get(uid string) ([]byte, error) {
 	return output, nil
 }
 
-func (u *userService) Create(user domain.User) (err error) {
-	err = u.UserRepository.Store(user)
+func (u *userService) Create(ctx context.Context, user domain.User) (err error) {
+	err = u.UserRepository.Store(ctx, user)
 	return
 }
 
-func (u *userService) Update(user domain.User) (err error) {
-	err = u.UserRepository.Update(user)
+func (u *userService) Update(ctx context.Context, user domain.User) (err error) {
+	err = u.UserRepository.Update(ctx, user)
 	return
 }
 
